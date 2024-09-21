c20e2a64cf69bebcb3cb96af341031dbb0ff06bd
Vladimir Nee <yimir7991@gmail.com>
2024-09-22 00:11:04 +0500

    init: vue3 frontend init

---
+++ Presentation/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// import { RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="w-full flex justify-center items-center">
+    Header
+  </div>
+
+  <RouterView />
+</template>
